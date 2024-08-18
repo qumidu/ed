fd7a50d279ec089ed8e4b9de1dfdd10db62f9a79
Duong <duongquach.lldtek@gmail.com>
2024-08-18 14:55:56 +0700

    1. navbar. hero. categories

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,4 @@
-// blur navbar
 const navbar = document.querySelector(`.navbar`);
-
-window.addEventListener(`scroll`, () => {
-  if (window.scrollY > 50) {
-    navbar.classList.add(`blur-navbar`);
-    hideNavbar();
-  } else {
-    navbar.classList.remove(`blur-navbar`);
-  }
-});
-
 const navLinks = document.querySelectorAll(`.nav-link`);
 const navbarCollapse = document.querySelector(`.navbar-collapse.collapse`);
 const navbarToggler = document.querySelector(`.navbar-toggler`);
@@ -21,12 +10,48 @@ function hideNavbar() {
   navbarToggler.classList.add(`collapsed`);
 }
 
-navLinks.forEach((navLink) => {
-  navLink.addEventListener(`click`, hideNavbar);
-});
-main.addEventListener(`click`, hideNavbar);
-navbarBrand.addEventListener(`click`, hideNavbar);
-navbarToggler.addEventListener(`click`, () => {
+function hideMenuToggle() {
+  if (navbar.classList.contains(`blur-navbar`)) {
+    navbar.classList.remove(`blur-navbar`);
+    navbar.classList.add(`blur-navbar-2`);
+    main.classList.add(`blur-main`);
+  } else if (navbar.classList.contains(`blur-navbar-2`)) {
+    navbar.classList.remove(`blur-navbar-2`);
+    navbar.classList.add(`blur-navbar`);
+    main.classList.remove(`blur-main`);
+  } else {
+    navbar.classList.add(`blur-navbar-2`);
+    main.classList.add(`blur-main`);
+  }
+}
+
+function scrollAndChoose() {
   navbar.classList.add(`blur-navbar`);
-  navbar.classList.add(`pb-0`);
+  navbar.classList.remove(`blur-navbar-2`);
+  main.classList.remove(`blur-main`);
+}
+
+window.addEventListener(`scroll`, () => {
+  if (window.scrollY > 50) {
+    scrollAndChoose();
+    hideNavbar();
+  } else {
+    navbar.classList.remove(`blur-navbar`);
+  }
 });
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener(`click`, () => {
+    hideNavbar();
+    scrollAndChoose();
+  });
+});
+
+main.addEventListener(`click`, () => {
+  hideNavbar();
+  scrollAndChoose();
+});
+
+navbarBrand.addEventListener(`click`, hideNavbar);
+
+navbarToggler.addEventListener(`click`, hideMenuToggle);
